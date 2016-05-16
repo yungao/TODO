97be4bd55adf13e9390589f4a7c76e169d9142b0
gaoyun <gaoyun@eli-tech.com>
2016-05-16 15:50:14 +0800

    xx

--- src/models/session.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type UserSession struct {
-    Id            int64  `form:"id" db:"id"`
-    Username      string `form:"name" db:"username"`
-    Password      string `form:"password" db:"password"`
-    authenticated bool   `form:"-" db:"-"`
-}
